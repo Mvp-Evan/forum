9eb49fc494a3a0544bb0208835a8185e1186fcff
Evan <299909@university-365.com>
2021-05-21 19:48:03 +0800

    add react

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static('src'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
